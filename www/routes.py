f132f3ec2e3db10dc18813d0e3c4e8a0ad9ac086
ActionLS <opil94@yandex.ru>
2022-01-26 17:39:35 +0300

    Tables for all intervals

--- www/routes.py
+++ www/routes.py
@@ -21,9 +21,25 @@ def overview():
 
 @app.route('/indicators')
 def indicators():
-    df = pd.read_pickle(os.path.join('data', 'indicators.pkl'))
+    df_min1 = pd.read_pickle(os.path.join('data', '1min.pkl'))
+    df_min5 = pd.read_pickle(os.path.join('data', '5min.pkl'))
+    df_min15 = pd.read_pickle(os.path.join('data', '15min.pkl'))
+    df_min30 = pd.read_pickle(os.path.join('data', '30min.pkl'))
+    df_hour = pd.read_pickle(os.path.join('data', 'hour.pkl'))
+    df_day = pd.read_pickle(os.path.join('data', 'day.pkl'))
+    df_week = pd.read_pickle(os.path.join('data', 'week.pkl'))
+    df_month = pd.read_pickle(os.path.join('data', 'month.pkl'))
+    classes = 'table table-striped table-bordered table-hover table-sm'
     return render_template('indicators.html', title='Indicators',
-                           table=df.to_html(classes='table table-striped table-bordered table-hover table-sm'))
+                           table_min1=df_min1.to_html(classes=classes),
+                           table_min5=df_min5.to_html(classes=classes),
+                           table_min15=df_min15.to_html(classes=classes),
+                           table_min30=df_min30.to_html(classes=classes),
+                           table_hour=df_hour.to_html(classes=classes),
+                           table_day=df_day.to_html(classes=classes),
+                           table_week=df_week.to_html(classes=classes),
+                           table_month=df_month.to_html(classes=classes)
+                           )
 
 
 @app.before_request
