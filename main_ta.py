b9a247f982fc764df43093f31aee71ac3487625a
ActionLS <opil94@yandex.ru>
2022-01-26 17:41:49 +0300

    Updating candles and indicators while streaming,

--- main_ta.py
+++ main_ta.py
@@ -1,14 +1,21 @@
 import asyncio
 from ta.scanner import Scanner
+import ta.myhelper as mh
+from ta.schemas import intervals
 import os
+from ta.scanner import test
+
+paths = [os.path.join('data', interval + '.pkl') for interval in intervals]
 
 
 if __name__ == "__main__":
+    # test()
+
     scanner = Scanner()
-    scanner.fill_all_stocks()
-    scanner.sum_df()
-    scanner.print_dfs()
-    scanner.save_df(os.path.join('data', 'indicators.pkl'))
+    scanner.save_df(paths)
+    scanner.print_df()
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(scanner.streaming())
+    loop.create_task(scanner.streaming())
+    loop.create_task(scanner.resave_df(paths))
+    loop.run_forever()
--- ta/scanner.py
+++ ta/scanner.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, datetime
 from concurrent.futures import ThreadPoolExecutor
 import asyncio
 
@@ -6,46 +6,104 @@ import tinvest as ti
 import pandas as pd
 
 from ta import myhelper as mh
+from ta.stock import Stock
+from ta.schemas import intervals, Interval
+
+
+TITLE = [
+    'Ticker', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume',
+    'EMA10', 'EMA20', 'EMA100', 'EMA200',
+    'MACD_hist', 'RSI', 'ATR10'
+    ]
+
+
+def update_raw(df: pd.DataFrame, payload: ti.CandleStreaming) -> pd.DataFrame:
+    df.loc[-1, 'Time'] = payload.time
+    df.loc[-1, 'Open'] = payload.o
+    df.loc[-1, 'High'] = payload.h
+    df.loc[-1, 'Low'] = payload.l
+    df.loc[-1, 'Close'] = payload.c
+    df.loc[-1, 'Volume'] = payload.v
+    return df
+
+
+async def handle_candle(payload: ti.CandleStreaming, stock: Stock):
+    tf = stock.timeframes[payload.interval]
+    if tf.df['Time'].iat[-1] < payload.time:
+        print(f'{stock.ticker}: New row')
+        tf.df.append(pd.Series(), ignore_index=True)
+        tf.df = update_raw(tf.df, payload)
+    elif tf.df['Time'].iat[-1] == payload.time:
+        print(f'{stock.ticker}: Update this raw')
+        tf.df = update_raw(tf.df, payload)
+
+
+def test():
+    client = mh.get_client()
+    p = client.get_market_search_by_ticker('SPCE').payload.instruments[0]
+    s = Stock(ticker=p.ticker, figi=p.figi, isin=p.isin, currency=p.currency)
+    tf = s.timeframes[Interval.week]
+    s.fill_df(client, tf)
+    print(tf.df)
 
 
 class Scanner:
     def __init__(self):
         self.client = mh.get_client()
         self.usd_stocks = mh.get_market_data(self.client, 'USD', developing=True)
-        self.df = pd.DataFrame(columns=['Ticker', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume',
-                                        'SMA10', 'SMA20', 'SMA100', 'SMA200',
-                                        'EMA10', 'EMA20', 'EMA100', 'EMA200',
-                                        'MACD_hist', 'RSI', 'ATR10'])
-        self.fill_all_stocks()
 
-    def fill_all_stocks(self):
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            [executor.submit(s.fill_df, self.client, s.m1) for s in self.usd_stocks.values()]
-            [executor.submit(s.fill_indicators, s.m1) for s in self.usd_stocks.values()]
+        self.fill_dfs()
+        self.fill_indicators()
+        self.summeries = [self.sum_df(interval) for interval in intervals]
 
-    def update_indicators(self):
-        for s in self.usd_stocks.values():
-            s.fill_indicators(s.m1)
+    def fill_dfs(self):
+        for interval in intervals:
+            with ThreadPoolExecutor(max_workers=6) as executor:
+                [executor.submit(s.fill_df, self.client, s.timeframes[interval]) for s in self.usd_stocks.values()]
 
-    def sum_df(self):
-        last_values = [[s.ticker] + s.m1.df.tail(1).values.tolist()[-1]
+    def fill_indicators(self):
+        for interval in intervals:
+            with ThreadPoolExecutor(max_workers=6) as executor:
+                [executor.submit(s.fill_indicators, s.timeframes[interval]) for s in self.usd_stocks.values()]
+
+    async def update_indicators(self):
+        for interval in intervals:
+            with ThreadPoolExecutor(max_workers=6) as executor:
+                [executor.submit(s.fill_indicators, s.timeframes[interval])
+                 for s in self.usd_stocks.values()]
+
+    def sum_df(self, interval: ti.CandleResolution) -> pd.DataFrame:
+        last_values = [[s.ticker] + s.timeframes[interval].df.tail(1).values.tolist()[-1]
                        for s in self.usd_stocks.values()]
-        self.df = pd.DataFrame(last_values, columns=self.df.columns)\
-                  .sort_values(by='Ticker', ascending=True, ignore_index=True)
+        print(f"{interval} Done")
+        return pd.DataFrame(last_values, columns=TITLE).sort_values(
+            by='Ticker', ascending=True, ignore_index=True
+        )
 
     async def streaming(self):
         async with ti.Streaming(mh.get_token()) as streaming:
-            await asyncio.gather(*(streaming.candle.subscribe(s.figi, s.m1.interval)
-                                   for s in self.usd_stocks.values()))
+            for interval in intervals:
+                await asyncio.gather(*(streaming.candle.subscribe(s.figi, interval)
+                                       for s in self.usd_stocks.values()))
             async for event in streaming:
-                print(event)
+                await handle_candle(event.payload, self.usd_stocks[event.payload.figi])
 
-    def print_dfs(self):
-        print(self.df.to_string())
+    def print_df(self) -> None:
+        for df in self.summeries:
+            print(df.to_string())
 
-    def save_df(self, path):
-        self.df.to_pickle(path)
+    def save_df(self, paths) -> None:
+        for i in range(len(intervals)):
+            self.summeries[i].to_pickle(paths[i])
 
+    async def resave_df(self, paths):
+        while True:
+            print('Resave function')
+            await asyncio.sleep(30)
+            await self.update_indicators()
+            [self.sum_df(interval) for interval in intervals]
+            self.save_df(paths)
+            self.print_df()
 
 
 def overview():
