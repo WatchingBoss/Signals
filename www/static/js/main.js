63e7c4bed327c1a8a9fba56145f0d746b550ec57
ActionLS <opil94@yandex.ru>
2022-02-04 19:41:37 +0300

    All scripts is main.js file

---
+++ www/static/js/main.js
@@ -0,0 +1,20 @@
+// Change timeframe
+let interval_select = document.getElementById('interval')
+interval_select.onchange = function(){
+    let current_interval = window.location.href.split('/').pop();
+    window.location.assign(window.location.href.replace(current_interval, interval_select.value))
+}
+
+// Sort table columns
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    const tbody = table.querySelector('tbody');
+    Array.from(tbody.querySelectorAll('tr'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => tbody.appendChild(tr) );
+})));
