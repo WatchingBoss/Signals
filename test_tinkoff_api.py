869b7879602dec9ba99faba235f441c4b6aa1899
ActionLS <opil94@yandex.ru>
2025-11-14 08:50:27 +0300

    Store dict of rub shares and load in separated function

--- test_tinkoff_api.py
+++ test_tinkoff_api.py
@@ -5,26 +5,33 @@ from tinkoff.invest.sandbox.client import SandboxClient
 from tinkoff.invest import InstrumentIdType, InstrumentType
 
 
+DATA_DIR_PATH = os.path.join(os.curdir, "data")
+SHARES_FILE_PATH = os.path.join(DATA_DIR_PATH, "shares.json")
+
+
 def get_token() -> str:
     with open(os.path.join(os.path.expanduser('~'), 'no_commit', 'info.json')) as f:
         data = json.load(f)
     return data['token_tinkoff_sandbox']
 
 
+def get_rub_shares(client) -> dict:
+    if not os.path.isdir(DATA_DIR_PATH):
+        os.mkdir(DATA_DIR_PATH)
+    if not os.path.isfile(SHARES_FILE_PATH):
+        rub_shares = {x.ticker: (x.figi, x.class_code, x.name)
+                      for x in client.instruments.shares().instruments if x.currency == "rub"}
+        with open(SHARES_FILE_PATH, 'w') as f:
+            json.dump(rub_shares, f)
+    with open(SHARES_FILE_PATH, 'r') as f:
+        return json.load(f)
+
+
 def main():
     api_token = get_token()
     with SandboxClient(api_token) as client:
-        instrument_gazp = client.instruments.get_instrument_by(id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
-                                                 class_code="TQBR",
-                                                 id="GAZP")
-        all_shares = client.instruments.shares()
-
-        rub_shares = [x for x in all_shares.instruments if x.currency == "rub"]
-        i = 1
-        for share in rub_shares:
-            print(f"{i:3}: | {share.figi:12} | {share.ticker:6} | {share.class_code:6} | {share.name}")
-            i += 1
-
+        shares = get_rub_shares(client)
+        print(f"{shares['SBER']}")
 
 
 if __name__ == "__main__":
