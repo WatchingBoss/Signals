8f40c0e1b483107da37e9ca3c1c284f5e62173de
ActionLS <opil94@yandex.ru>
2025-11-14 10:51:24 +0300

    Get historical candles and print time and price values

--- test_tinkoff_api.py
+++ test_tinkoff_api.py
@@ -1,12 +1,18 @@
 import os
 import json
+from datetime import datetime
+import pytz
 
 from tinkoff.invest.sandbox.client import SandboxClient
-from tinkoff.invest import InstrumentIdType, InstrumentType
+from tinkoff.invest import CandleInterval
+from tinkoff.invest.utils import now
 
 
 DATA_DIR_PATH = os.path.join(os.curdir, "data")
 SHARES_FILE_PATH = os.path.join(DATA_DIR_PATH, "shares.json")
+
+TARGET_TZ = pytz.timezone("Europe/Moscow")
+NANO_DIVISOR = 1_000_000_000
 
 
 def get_token() -> str:
@@ -31,7 +37,17 @@ def main():
     api_token = get_token()
     with SandboxClient(api_token) as client:
         shares = get_rub_shares(client)
-        print(f"{shares['SBER']}")
+        sber_candles = client.market_data.get_candles(
+            instrument_id=shares['SBER'][0],
+            to=now(),
+            limit=21,
+            interval=CandleInterval.CANDLE_INTERVAL_5_MIN,
+        ).candles
+        for candle in sber_candles:
+            moscow_time = candle.time.astimezone(TARGET_TZ).strftime("%H:%M")
+            close_price = candle.close.units + (candle.close.nano / NANO_DIVISOR)
+            print(f"{moscow_time} | {close_price}")
+
 
 
 if __name__ == "__main__":
