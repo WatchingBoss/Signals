6f4b2884007f6ec38e6e761aa6417919b8fd1d9f
ActionLS <opil94@yandex.ru>
2025-11-16 07:57:35 +0300

    Use uid instead of figi, add stream subscription

--- test_tinkoff_api.py
+++ test_tinkoff_api.py
@@ -1,12 +1,20 @@
 import os
 import json
+import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
 import polars as pl
 
 from tinkoff.invest.sandbox.client import SandboxClient
-from tinkoff.invest import CandleInterval
+from tinkoff.invest import (
+    CandleInterval,
+    MarketDataRequest,
+    SubscribeCandlesRequest,
+    SubscriptionAction,
+    SubscriptionInterval,
+    CandleInstrument,
+)
 from tinkoff.invest.utils import now
 
 
@@ -23,22 +31,48 @@ def get_token() -> str:
     return data['token_tinkoff_sandbox']
 
 
+def request_iterator(uid: str, interval: SubscriptionInterval):
+    yield MarketDataRequest(
+        subscribe_candles_request=SubscribeCandlesRequest(
+            waiting_close=True,
+            subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+            # candle_source_type=CandleSource.CANDLE_SOURCE_EXCHANGE,
+            instruments=[
+                CandleInstrument(
+                    instrument_id=uid,
+                    interval=interval,
+                )
+            ],
+        )
+    )
+    while True:
+        time.sleep(1)
+
+
 def get_rub_shares(client) -> dict:
     if not os.path.isdir(DATA_DIR_PATH):
         os.mkdir(DATA_DIR_PATH)
     if not os.path.isfile(SHARES_FILE_PATH):
-        rub_shares = {x.ticker: (x.figi, x.class_code, x.name)
-                      for x in client.instruments.shares().instruments if x.currency == "rub"}
+        rub_shares = {
+            x.ticker: {
+                'figi': x.figi,
+                'uid': x.uid,
+                'class_code': x.class_code,
+                'name': x.name,
+                'isin': x.isin
+            }
+            for x in client.instruments.shares().instruments if x.currency == "rub"
+        }
         with open(SHARES_FILE_PATH, 'w') as f:
             json.dump(rub_shares, f)
     with open(SHARES_FILE_PATH, 'r') as f:
         return json.load(f)
 
 
-def get_candles_with_limit(client, figi: str, to_time: datetime, limit: int,
+def get_candles_with_limit(client, uid: str, to_time: datetime, limit: int,
                            interval: CandleInterval) -> pl.DataFrame:
     raw_candles = client.market_data.get_candles(
-        instrument_id=figi,
+        instrument_id=uid,
         to=to_time,
         limit=limit,
         interval=interval,
@@ -72,9 +106,14 @@ def main():
     api_token = get_token()
     with SandboxClient(api_token) as client:
         shares = get_rub_shares(client)
-        sber_candles = get_candles_with_limit(client, shares['SBER'][0], now(), 50,
-                                              CandleInterval.CANDLE_INTERVAL_5_SEC)
+        sber_candles = get_candles_with_limit(
+            client, shares['SBER']['uid'], now(), 25,CandleInterval.CANDLE_INTERVAL_1_MIN
+        )
         print_candles(sber_candles)
+        for metadata in client.market_data_stream.market_data_stream(
+            request_iterator(shares['SBER']['uid'], SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE)
+        ):
+            print(metadata)
 
 
 if __name__ == "__main__":
