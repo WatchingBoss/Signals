67a98b2f9bb5a17d8377bdbbe084ec5c2f7726ec
ActionLS <opil94@yandex.ru>
2025-11-20 21:53:51 +0300

    Store bonds data and print. Add Bond Class

--- profile.py
+++ profile.py
@@ -1,6 +1,5 @@
 import os
 import json
-from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from tinkoff.invest import Client
@@ -20,27 +19,59 @@ def get_token() -> str:
     return data['token_tinkoff_view_only']
 
 
+class Bond:
+    def __init__(
+            self, ticker: str, uid: str, quantity: float, average_position_price: float,
+            expected_yield: float, nkd: float, current_price: float,
+    ):
+        self.ticker = ticker
+        self.uid = uid
+        self.quantity = quantity
+        self.average_position_price = average_position_price
+        self.expected_yield = expected_yield
+        self.nkd = nkd
+        self.current_price = current_price
+        self.total = quantity * current_price
+        self.total_nkd = quantity * nkd
+
+    def out_string(self) -> str:
+        return str(f"{self.ticker:<18} {self.quantity:<8} {self.average_position_price:<14.2f} "
+                   f"{self.expected_yield:<12} {self.nkd:<7} {self.current_price:<12.2f} "
+                   f"{self.total:<12.2f} {self.total_nkd:<12.2f}")
+        
+
+
 def main():
     with Client(get_token()) as client:
         accounts = client.users.get_accounts().accounts
-        for account in accounts:
-            print(f"Account ID: {account.id}")
-            print(f"Account Type: {account.type}")
-            print(f"Account Name: {account.name}")
-            print(f"Account Access Level: {account.access_level}")
-            print("\n-----------------------------------------------\n")
+
+        account_total = {}
+        bonds_total = {}
+        bonds = []
 
         for account in accounts:
             portfolio = client.operations.get_portfolio(account_id=account.id)
-            print(f"{account.name}:")
-            print(f"Total amount: {get_price(portfolio.total_amount_portfolio)}")
-            print(f"{'Ticker':<18} {'Type':<8} {'Quantity':<8} {'NKD':<6} {'Yield':<8}")
+            account_total[account.name] = get_price(portfolio.total_amount_portfolio)
+            bonds_total[account.name] = get_price(portfolio.total_amount_bonds)
             for p in portfolio.positions:
-                print(
-                    f"{p.ticker:<18} {p.instrument_type:<8} {get_price(p.quantity):<8} "
-                    f"{get_price(p.current_nkd):<6} {get_price(p.expected_yield):<8}"
-                )
-            print("\n-----------------------------------------------\n")
+                if p.instrument_type == "bond":
+                    bonds.append(Bond(
+                        p.ticker, p.instrument_uid, get_price(p.quantity), get_price(p.average_position_price),
+                        get_price(p.expected_yield), get_price(p.current_nkd), get_price(p.current_price)
+                    ))
+
+        for key, value in account_total.items():
+            print(f"{key:<18} : {value}\n"
+                  f"{'Bonds':>19}: {bonds_total[key]}")
+        print(f"All accounts total: {sum(account_total.values())}")
+        print(f"{'Bonds total':>18}: {sum(bonds_total.values())}")
+
+        print("Bonds:")
+        print(f"{'Ticker':<18} {'Quantity':<8} {'Average Price':<14} {'Yield':<12} {'NKD':<7} {'Price':<12}"
+              f"{'Total':<12} {'Total NKD':<12}")
+        for bond in bonds:
+            print(bond.out_string())
+
 
 
 if __name__ == "__main__":
